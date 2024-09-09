940b2c3d2bd1aa0c067b1785c1ba2b267afa833b
Nathnael Yirga <Nathnaelyirga@gmail.com>
2024-09-09 12:41:25 +0300

    erroerous meaty skeleton, to be deployed for api key registration form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import Row from "./components/Row";
+import requests from "./utils/requests";
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="App">
+			<Nav />
+			<Banner />
+			<Row
+				title="NETFLIX ORIGINALS"
+				fetchUrl={requests.fetchNetflixOriginals}
+				isLargeRow
+			/>
+			<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+			<Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row
+				title="Documentaries Movies"
+				fetchUrl={requests.fetchDocumentaries}
+			/>
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
